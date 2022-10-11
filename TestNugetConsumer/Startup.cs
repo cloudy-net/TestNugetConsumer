a39b0cccddf9a05558f6659a2e567af38e96cc1a
Björn Göransson <bjorn.a.goransson@gmail.com>
2022-10-11 20:21:19 +0200

    Convert to top level statement program

--- TestNugetConsumer/Startup.cs
+++ TestNugetConsumer/Startup.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Hosting;
 using TestNugetConsumer.Models;
@@ -10,53 +8,41 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Http;
 using Cloudy.CMS.Routing;
 using Cloudy.CMS.UI;
+using System.IO;
 
-namespace TestNugetConsumer
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages().AddApplicationPart(typeof(CloudyUIAssemblyHandle).Assembly);
+builder.Services.AddMvc();
+builder.Services.AddCloudy(cloudy => cloudy
+    .AddAdmin(admin => admin.Unprotect())
+    .AddContext<PageContext>()
+);
+builder.Services.AddDbContext<PageContext>(options => options
+    .UseInMemoryDatabase("cloudytest")
+);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
 {
-    public class Startup
-    {
-        IConfiguration Configuration { get; }
-
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddRazorPages().AddApplicationPart(typeof(CloudyUIAssemblyHandle).Assembly);
-            services.AddMvc();
-            services.AddCloudy(cloudy => cloudy
-                .AddAdmin(admin => admin.Unprotect())
-                .AddContext<PageContext>()
-            );
-            services.AddDbContext<PageContext>(options => options
-                .UseInMemoryDatabase("cloudytest")
-            );
-        }
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseStaticFiles(new StaticFileOptions
-            {
-                OnPrepareResponse = ctx => ctx.Context.Response.Headers.Append("Cache-Control", $"no-cache")
-            });
-            app.UseRouting();
-            app.UseAuthentication();
-            app.UseAuthorization();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-                endpoints.MapGet("/", async c => c.Response.Redirect("/Admin"));
-                endpoints.MapGet("/pages", async c => await c.Response.WriteAsJsonAsync(c.RequestServices.GetService<PageContext>().Pages));
-                endpoints.MapGet("/pages/{route:contentroute}", async c => await c.Response.WriteAsync($"Hello {c.GetContentFromContentRoute<Page>().Name}"));
-                endpoints.MapControllerRoute(null, "/controllertest/{route:contentroute}", new { controller = "Page", action = "Index" });
-            });
-        }
-    }
+    app.UseDeveloperExceptionPage();
 }
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    OnPrepareResponse = ctx => ctx.Context.Response.Headers.Append("Cache-Control", $"no-cache")
+});
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapRazorPages();
+    endpoints.MapGet("/", async c => c.Response.Redirect("/Admin"));
+    endpoints.MapGet("/pages", async c => await c.Response.WriteAsJsonAsync(c.RequestServices.GetService<PageContext>().Pages));
+    endpoints.MapGet("/pages/{route:contentroute}", async c => await c.Response.WriteAsync($"Hello {c.GetContentFromContentRoute<Page>().Name}"));
+    endpoints.MapControllerRoute(null, "/controllertest/{route:contentroute}", new { controller = "Page", action = "Index" });
+});
+
+app.Run();
