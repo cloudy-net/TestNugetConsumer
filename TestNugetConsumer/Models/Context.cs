59e8edca7c9d7786e2f45bd35a8b5dd920474d28
Björn Göransson <bjorn.a.goransson@gmail.com>
2022-10-11 20:25:42 +0200

    Rename context

---
+++ TestNugetConsumer/Models/Context.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TestNugetConsumer.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) { }
+
+        public DbSet<Page> Pages { get; set; }
+    }
+}
--- TestNugetConsumer/Startup.cs
+++ TestNugetConsumer/Startup.cs
@@ -16,9 +16,9 @@ builder.Services.AddRazorPages().AddApplicationPart(typeof(CloudyUIAssemblyHandl
 builder.Services.AddMvc();
 builder.Services.AddCloudy(cloudy => cloudy
     .AddAdmin(admin => admin.Unprotect())
-    .AddContext<PageContext>()
+    .AddContext<Context>()
 );
-builder.Services.AddDbContext<PageContext>(options => options
+builder.Services.AddDbContext<Context>(options => options
     .UseInMemoryDatabase("cloudytest")
 );
 
@@ -40,7 +40,7 @@ app.UseEndpoints(endpoints =>
 {
     endpoints.MapRazorPages();
     endpoints.MapGet("/", async c => c.Response.Redirect("/Admin"));
-    endpoints.MapGet("/pages", async c => await c.Response.WriteAsJsonAsync(c.RequestServices.GetService<PageContext>().Pages));
+    endpoints.MapGet("/pages", async c => await c.Response.WriteAsJsonAsync(c.RequestServices.GetService<Context>().Pages));
     endpoints.MapGet("/pages/{route:contentroute}", async c => await c.Response.WriteAsync($"Hello {c.GetContentFromContentRoute<Page>().Name}"));
     endpoints.MapControllerRoute(null, "/controllertest/{route:contentroute}", new { controller = "Page", action = "Index" });
 });
